eea64f7e1b2a78a30ca35fdb405d851233b1ec5b
Nicolas Merget <>
2022-09-07 23:22:02 +0200

    feat: add capacitor

---
+++ android/app/src/main/java/merget/nicolas/pkmzoggen/MainActivity.java
@@ -0,0 +1,5 @@
+package merget.nicolas.pkmzoggen;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'merget.nicolas.pkmzoggen',
+  appName: 'pokemon-zoggen',
+  webDir: 'build',
+  bundledWebRuntime: false,
+};
+
+export default config;
